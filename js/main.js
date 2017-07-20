8259e55c64209bcf969d4d695f8fc23e7ddc8cea
Haochen Wei <cweihc@gmail.com>
2017-07-19 22:51:33 -0400

    Carson 让Header起码开始工作

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
-
+    /*Header reshape*/
+    $(function(){
+        $(window).scroll(function(){
+            if($(this).scrollTop() > 600) {
+                $('.header1').slideUp(300);
+                $('img.bigLogo').attr('src','media/images/header/smallLogo.jpg');
+                $('.menuLeft, .menuRight, .teleText').addClass('revisedHeader');
+                $('.toggle').addClass('revisedHeader2');
+                $('.telephone').addClass('revisedHeader3');
+                $('.toggleM, .headerSubM').addClass('revisedHeader');
+            }
+            else {
+                $('.header1').slideDown(300);
+                $('img.bigLogo').attr('src','media/images/header/logo.png');
+                $('.menuLeft, .menuRight, .teleText').removeClass('revisedHeader');
+                $('.toggle').removeClass('revisedHeader2');
+                $('.telephone').removeClass('revisedHeader3');
+                $('.toggleM, .headerSubM').removeClass('revisedHeader');
+            }
+        });
+    });
 });
-/*Header auto re-shape required*/
