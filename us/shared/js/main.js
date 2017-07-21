9042420b7d2e536821981d8bed97926e62c666cd
Haochen Wei <cweihc@gmail.com>
2017-07-21 10:17:26 -0400

    Carson 修复Footer多次出发事件，修改文件结构

--- us/shared/js/main.js
+++ us/shared/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         $(window).scroll(function(){
             if($(this).scrollTop() > 500) {
                 $('.header1').slideUp(300);
-                $('img.bigLogo').attr('src','media/images/header/smallLogo.jpg');
+                $('img.bigLogo').attr('src','../shared/images/header/smallLogo.jpg');
                 $('.menuLeft, .menuRight, .teleText').addClass('revisedHeader');
                 $('.toggle').addClass('revisedHeader2');
                 $('.telephone').addClass('revisedHeader3');
@@ -12,7 +12,7 @@ $(document).ready(function(){
             }
             else {
                 $('.header1').slideDown(300);
-                $('img.bigLogo').attr('src','media/images/header/logo.png');
+                $('img.bigLogo').attr('src','../shared/images/header/logo.png');
                 $('.menuLeft, .menuRight, .teleText').removeClass('revisedHeader');
                 $('.toggle').removeClass('revisedHeader2');
                 $('.telephone').removeClass('revisedHeader3');
