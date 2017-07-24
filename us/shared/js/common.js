4ef2ceaac9ae566e69e58b632a5c4460c20d6739
Haochen Wei <cweihc@gmail.com>
2017-07-24 14:56:27 -0400

    Carson 主页图标mouseover效果加入

--- us/shared/js/common.js
+++ us/shared/js/common.js
@@ -20,6 +20,44 @@ $(document).ready(function(){
             }
         });
     });
+
+    /*index page image hover replace*/
+    $('img.contactIcon1').mouseover(function(){
+        $(this).attr('src','../shared/images/index/chs_contactIcon1Blue.png');
+    }).mouseleave(function(){
+        $(this).attr('src','../shared/images/index/chs_contactIcon1.png');
+    });
+    $('img.contactIcon2').mouseover(function(){
+        $(this).attr('src','../shared/images/index/chs_contactIcon2Blue.png');
+    }).mouseleave(function(){
+        $(this).attr('src','../shared/images/index/chs_contactIcon2.png');
+    });
+    $('img.contactIcon3').mouseover(function(){
+        $(this).attr('src','../shared/images/index/chs_contactIcon3Blue.png');
+    }).mouseleave(function(){
+        $(this).attr('src','../shared/images/index/chs_contactIcon3.png');
+    });
+
+    $('.social1').mouseover(function(){
+        $(this).attr('src','../shared/images/index/iconWechatBlue.jpg');
+    }).mouseleave(function(){
+        $(this).attr('src','../shared/images/index/iconWechat.jpg');
+    });
+    $('.social2').mouseover(function(){
+        $(this).attr('src','../shared/images/index/iconWeiboBlue.jpg');
+    }).mouseleave(function(){
+        $(this).attr('src','../shared/images/index/iconWeibo.jpg');
+    });
+    $('.social3').mouseover(function(){
+        $(this).attr('src','../shared/images/index/iconFacebookBlue.jpg');
+    }).mouseleave(function(){
+        $(this).attr('src','../shared/images/index/iconFacebook.jpg');
+    });
+    $('.social4').mouseover(function(){
+        $(this).attr('src','../shared/images/index/iconInsBlue.jpg');
+    }).mouseleave(function(){
+        $(this).attr('src','../shared/images/index/iconIns.jpg');
+    });
 });
 
 
