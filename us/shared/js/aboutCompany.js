bbc796774bf40fac3b3ce6e6c63d3fa3acd8f17e
Haochen Wei <cweihc@gmail.com>
2017-08-10 18:31:49 -0400

    Carson 一堆废代码

--- us/shared/js/aboutCompany.js
+++ us/shared/js/aboutCompany.js
@@ -4,4 +4,51 @@ $(document).ready(function(){
         speed: 1000,
         reverse: 'false'
     });
+
+    $('.bottomHover1').mouseover(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalBottomHover1.jpg');
+    }).mouseleave(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalAll.jpg');
+    });
+    $('.bottomHover2').mouseover(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalBottomHover2.jpg');
+    }).mouseleave(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalAll.jpg');
+    });
+    $('.bottomHover3').mouseover(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalBottomHover3.jpg');
+    }).mouseleave(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalAll.jpg');
+    });
+    $('.bottomHover4').mouseover(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalBottomHover4.jpg');
+    }).mouseleave(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalAll.jpg');
+    });
+    $('.usa').click(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalHoverUSA.jpg');
+    }).mouseleave(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalAll.jpg');
+    });
+    $('.canada').click(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalHoverCanada.jpg');
+    }).mouseleave(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalAll.jpg');
+    });
+    $('.australia').click(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalHoverAustarlia.jpg');
+    }).mouseleave(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalAll.jpg');
+    });
+    $('.singapore').click(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalHoverSingapore.jpg');
+    }).mouseleave(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalAll.jpg');
+    });
+    $('.china').click(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalHoverChina.jpg');
+    }).mouseleave(function(){
+        $('.globalImg').attr('src', '../shared/images/about/aboutGlobalAll.jpg');
+    })
+
 });
