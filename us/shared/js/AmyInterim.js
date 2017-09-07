68ccb5f69fd81ded04127f26eed19248ff48b5c8
Amy <carsonweic@gmail.com>
2017-09-07 20:35:00 +0800

    Amy 同下 及 共用样式完善

--- us/shared/js/AmyInterim.js
+++ us/shared/js/AmyInterim.js
@@ -1,11 +1,25 @@
 function show(foldTxt,imgP){
-	if(document.all(foldTxt).style.display=='none'){
-	   document.all(foldTxt).style.display='block';
-	   document.all(imgP).src='../shared/images/support/doubleArrowOrangeUp.png'
-	   }
-	else{
-	   document.all(foldTxt).style.display='none';	
-	   document.all(imgP).src='../shared/images/support/doubleArrowOrangeDown.png'}
-	   }
+
+	 var fo = $('#'+foldTxt);
+	 var f2 = $('#'+imgP);
+	(!fo.is(":visible")) ? (
+	   fo.show(),
+	   f2.attr("src",'../shared/images/support/doubleArrowOrangeUp.png'),
+	  $(document).bind("mouseup",(function(e){
+	   	setTimeout(function(){
+       	var _c = $('#'+foldTxt);   
+	     ((!_c.is(e.target) && _c.has(e.target).length === 0) && $('#'+foldTxt).is(":visible"))?
+	     (
+	     	$('#'+foldTxt).hide(),$(document).unbind("mouseup"),
+	     	$('#'+imgP).attr("src",'../shared/images/support/doubleArrowOrangeDown.png')
+	     ):"";
+		},1);
+		}))
+	   ) :
+	  (
+	   fo.hide(),$(document).unbind("mouseup"),
+	   f2.attr("src",'../shared/images/support/doubleArrowOrangeDown.png')
+	   )
+	}
 
 
