cf0eea9cf5cb14c6d1197733e5b50a1559b8f6d6
Amy <carsonweic@gmail.com>
2017-09-08 17:30:45 +0800

    Amy 同下

--- us/shared/js/AmyInterim.js
+++ us/shared/js/AmyInterim.js
@@ -4,21 +4,21 @@ function show(foldTxt,imgP){
 	 var f2 = $('#'+imgP);
 	(!fo.is(":visible")) ? (
 	   fo.show(),
-	   f2.attr("src",'../shared/images/support/doubleArrowOrangeUp.png'),
+	   f2.attr("src",'../shared/images/icons/doubleArrowOrangeUp.png'),
 	  $(document).bind("mouseup",(function(e){
 	   	setTimeout(function(){
        	var _c = $('#'+foldTxt);   
 	     ((!_c.is(e.target) && _c.has(e.target).length === 0) && $('#'+foldTxt).is(":visible"))?
 	     (
 	     	$('#'+foldTxt).hide(),$(document).unbind("mouseup"),
-	     	$('#'+imgP).attr("src",'../shared/images/support/doubleArrowOrangeDown.png')
+	     	$('#'+imgP).attr("src",'../shared/images/icons/doubleArrowOrangeDown.png')
 	     ):"";
 		},1);
 		}))
 	   ) :
 	  (
 	   fo.hide(),$(document).unbind("mouseup"),
-	   f2.attr("src",'../shared/images/support/doubleArrowOrangeDown.png')
+	   f2.attr("src",'../shared/images/icons/doubleArrowOrangeDown.png')
 	   )
 	}
 
