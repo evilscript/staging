09ca6abf186cbf930d41be12418b41ffcabdcf81
Amy <carsonweic@gmail.com>
2017-09-15 10:15:11 +0800

    Amy js效果修改

--- us/shared/js/AmyInterim.js
+++ us/shared/js/AmyInterim.js
@@ -1,25 +1,15 @@
+// hooyes 2017-09-14
 function show(foldTxt,imgP){
-
-	 var fo = $('#'+foldTxt);
-	 var f2 = $('#'+imgP);
-	(!fo.is(":visible")) ? (
-	   fo.show(),
-	   f2.attr("src",'../shared/images/icons/doubleArrowOrangeUp.png'),
+	 $(document).unbind("mouseup");
+     var img1 = '../shared/images/icons/doubleArrowOrangeUp.png';
+     var img2 ='../shared/images/icons/doubleArrowOrangeDown.png';
+	 var f1 = $('#'+foldTxt),f2 = $('#'+imgP);
+	 f1.hasClass("H-D") ? "" : f1.addClass("H-D");f2.hasClass("X-D") ? "" : f2.addClass("X-D");
+	 !f1.is(":visible") ? ( f1.addClass("H-ON"),f2.addClass("X-ON"),
 	  $(document).bind("mouseup",(function(e){
-	   	setTimeout(function(){
-       	var _c = $('#'+foldTxt);   
-	     ((!_c.is(e.target) && _c.has(e.target).length === 0) && $('#'+foldTxt).is(":visible"))?
-	     (
-	     	$('#'+foldTxt).hide(),$(document).unbind("mouseup"),
-	     	$('#'+imgP).attr("src",'../shared/images/icons/doubleArrowOrangeDown.png')
-	     ):"";
-		},1);
-		}))
-	   ) :
-	  (
-	   fo.hide(),$(document).unbind("mouseup"),
-	   f2.attr("src",'../shared/images/icons/doubleArrowOrangeDown.png')
-	   )
-	}
-
-
+	   	setTimeout(function(){(!f1.is(e.target) && f1.has(e.target).length === 0 && f1.is(":visible"))?
+	     (f1.hide(),f2.attr("src",img2),$(document).unbind("mouseup")):"";},1); }))) 
+	: (f1.removeClass("H-ON"),f2.removeClass("X-ON"))
+  	$(".H-D").hide();$(".H-ON").show().removeClass("H-ON");
+  	$(".X-D").attr("src",img2);$(".X-ON").attr("src",img1).removeClass("X-ON");
+}
