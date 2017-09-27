350c88bab5fb1aa180d28551cb2cf16b3956e578
Haochen Wei <cweihc@gmail.com>
2017-09-27 18:41:28 -0400

    Carson 可能出现的后端数据传输参数加入URL

--- us/shared/js/mobilePlanPriceDisplay.js
+++ us/shared/js/mobilePlanPriceDisplay.js
@@ -6,6 +6,7 @@ app.controller('mobilePlanController', function($scope) {
     $scope.plan1_dec = '    ';
     $scope.plan2_int = '45';
     $scope.plan2_dec = '    ';
+    parent.location.hash = "?isUser=false?line=1";
     $scope.isUser = function(){
         $scope.user = true;
         $('.ifCustomerLeftText').removeClass('ifCustomerSelected');
@@ -59,52 +60,64 @@ app.controller('mobilePlanController', function($scope) {
             if ( lineNumber == 1) {
                 $scope.plan1_int = '20';
                 $scope.plan2_int = '35';
+                parent.location.hash = "?isUser=true?line=1";
             }
             else if ( lineNumber == 2) {
                 $scope.plan1_int = '40';
                 $scope.plan2_int = '70';
+                parent.location.hash = "?isUser=true?line=2";
             }
             else if ( lineNumber == 3) {
                 $scope.plan1_int = '55';
                 $scope.plan2_int = '105';
+                parent.location.hash = "?isUser=true?line=3";
             }
             else if ( lineNumber == 4) {
                 $scope.plan1_int = '70';
                 $scope.plan2_int = '140';
+                parent.location.hash = "?isUser=true?line=4";
             }
             else if ( lineNumber == 5) {
                 $scope.plan1_int = '85';
                 $scope.plan2_int = '175';
+                parent.location.hash = "?isUser=true?line=5";
             }
             else if ( lineNumber == 6) {
                 $scope.plan1_int = '100';
                 $scope.plan2_int = '210';
+                parent.location.hash = "?isUser=true?line=6";
             }
         }
         else if ( isUser == false ){
             if ( lineNumber == 1) {
                 $scope.plan1_int = '30';
                 $scope.plan2_int = '45';
+                parent.location.hash = "?isUser=false?line=1";
             }
             else if ( lineNumber == 2) {
                 $scope.plan1_int = '60';
                 $scope.plan2_int = '90';
+                parent.location.hash = "?isUser=false?line=2";
             }
             else if ( lineNumber == 3) {
                 $scope.plan1_int = '80';
                 $scope.plan2_int = '135';
+                parent.location.hash = "?isUser=false?line=3";
             }
             else if ( lineNumber == 4) {
                 $scope.plan1_int = '100';
                 $scope.plan2_int = '180';
+                parent.location.hash = "?isUser=false?line=4";
             }
             else if ( lineNumber == 5) {
                 $scope.plan1_int = '120';
                 $scope.plan2_int = '225';
+                parent.location.hash = "?isUser=false?line=5";
             }
             else if ( lineNumber == 6) {
                 $scope.plan1_int = '140';
                 $scope.plan2_int = '270';
+                parent.location.hash = "?isUser=false?line=6";
             }
         }
     };
