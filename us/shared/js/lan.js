f39288e484faf625cdf4b2c820ce0fe049c28e1a
Amy <hsun@net263.com>
2017-11-09 17:54:22 +0800

    Amy refer

---
+++ us/shared/js/lan.js
@@ -0,0 +1,38 @@
+﻿var lan = {
+    "zh-cn": {
+        pageTitle: "",
+        BBNTitle: "",
+        emailTitle: "",
+        emailListTitle: "请在下面填入您朋友的Email（每行一个，不要加逗号）",
+        sendBtnText: "发送",
+        needBBN: "请填写BB号码",
+        needEmail: "请填写Email",
+        needEmailList: "请填写Email列表，每行一个",
+        commissionDesc: "您也可以把这个注册链接发送给您的朋友，在他（她）成功注册后，便可领取您的推荐佣金：",
+        notUser: "您还不是注册用户？请到网站首页申请帐号。"
+    },
+    "zh-cht": {
+        pageTitle: "",
+        BBNTitle: "",
+        emailTitle: "",
+        emailListTitle: "請在下面填入您朋友的Email（每行一個，不要加逗號）",
+        sendBtnText: "發送",
+        needBBN: "請填寫BB號碼",
+        needEmail: "您還沒有填寫您的Email，它是必須的",
+        needEmailList: "請填寫Email列表，每行一個",
+        commissionDesc: "您也可以把這個註冊連結發送給您的朋友，在他（她）成功註冊後，便可領取您的推薦傭金：",
+        notUser: "您還不是註冊用戶？請到網站首頁申請帳號。"
+    },
+    "en": {
+        pageTitle: "",
+        BBNTitle: "",
+        emailTitle: "",
+        emailListTitle: "Enter email addresses of your friends and family you wish to refer (one email address per line)",
+        sendBtnText: "Submit",
+        needBBN: "BB Number Required",
+        needEmail: "Email Required",
+        needEmailList: "Friend email list required",
+        commissionDesc: "You may also send the URL below to your referees to sign up for iTalkBB's service. After your referee has been using iTalkBB's service for more than 30 days, you will receive $1 off your monthly bill for as long as your referee is using iTalkBB",
+        notUser: ""
+    }
+}
