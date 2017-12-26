c6dd134c483dffe6c8928f901325e89a624a330b
Unknown <905349916@qq.com>
2017-12-26 11:25:41 +0800

    更新

--- us/shared/js/service.js
+++ us/shared/js/service.js
@@ -4,14 +4,17 @@ var a =window.location.href.indexOf("chs/bundles-tv-phone");
 if(a>-1){
 		cartman('2499cheapm','6个月免费');
         cartman('2499cheap','6个月免费');
-
-
+    
+        ecic('.btp2499button','https://webaccount.italkbb.com/BBSignUp/index_USCN_CHS.html?act=USCN_BB_Online&cid=202125&opc=USCN2017V10&cob=1')
+        ecic('.mbtp2499button','https://webaccount.italkbb.com/BBSignUp/index_USCN_CHS.html?act=USCN_BB_Online&cid=202125&opc=USCN2017V10&cob=1')   
 }
 
 var b =window.location.href.indexOf("chs/home-phone-plans");
 if(b>-1){
 		cartman('2499cheapm','6个月免费');
         cartman('2499cheap','6个月免费');
+        ecic('.hpp2499button','https://webaccount.italkbb.com/BBSignUp/index_USCN_CHS.html?act=USCN_BB_Online&cid=202125&opc=USCN2017V10&cob=1')
+        ecic('.mhpp2499button','https://webaccount.italkbb.com/BBSignUp/index_USCN_CHS.html?act=USCN_BB_Online&cid=202125&opc=USCN2017V10&cob=1')
 
 }
 
@@ -21,7 +24,8 @@ var at =window.location.href.indexOf("cht/bundles-tv-phone");
 if(at>-1){
 		cartman('2499cheapm','6個月免費');
         cartman('2499cheap','6個月免費');
-
+        ecic('.btp2499button','https://webaccount.italkbb.com/BBSignUp/index_USCN_CHT.html?act=USCN_BB_Online&cid=202125&opc=USCN2017V10&cob=1')
+        ecic('.mbtp2499button','https://webaccount.italkbb.com/BBSignUp/index_USCN_CHT.html?act=USCN_BB_Online&cid=202125&opc=USCN2017V10&cob=1') 
 
 }
 
@@ -29,6 +33,8 @@ var bt =window.location.href.indexOf("cht/home-phone-plans");
 if(bt>-1){
 		cartman('2499cheapm','6個月免費');
         cartman('2499cheap','6個月免費');
+        ecic('.hpp2499button','https://webaccount.italkbb.com/BBSignUp/index_USCN_CHT.html?act=USCN_BB_Online&cid=202125&opc=USCN2017V10&cob=1')
+        ecic('.mhpp2499button','https://webaccount.italkbb.com/BBSignUp/index_USCN_CHT.html?act=USCN_BB_Online&cid=202125&opc=USCN2017V10&cob=1')
 
 }
 
@@ -38,6 +44,8 @@ var an =window.location.href.indexOf("en/bundles-tv-phone");
 if(an>-1){
 		cartman('2499cheapm','6 Months Free');
         cartman('2499cheap','6 Months Free');
+        ecic('.btp2499button','https://webaccount.italkbb.com/BBSignUp/index_USCN_EN.html?act=USCN_BB_Online&cid=202125&opc=USCN2017V10&cob=1')
+        ecic('.mbtp2499button','https://webaccount.italkbb.com/BBSignUp/index_USCN_EN.html?act=USCN_BB_Online&cid=202125&opc=USCN2017V10&cob=1')
 
 
 }
@@ -48,6 +56,8 @@ if(bn>-1){
 
 		cartman('2499cheapm','6 Months Free');
         cartman('2499cheap','6 Months Free');
+        ecic('.hpp2499button','https://webaccount.italkbb.com/BBSignUp/index_USCN_EN.html?act=USCN_BB_Online&cid=202125&opc=USCN2017V10&cob=1')
+        ecic('.mhpp2499button','https://webaccount.italkbb.com/BBSignUp/index_USCN_EN.html?act=USCN_BB_Online&cid=202125&opc=USCN2017V10&cob=1')
         
 
 }
@@ -61,6 +71,10 @@ function cartman (id,text) {
 }
 
 
+function ecic (aa,url) {
+	 $(aa).attr("href",url)
+
+}
 
 
 
